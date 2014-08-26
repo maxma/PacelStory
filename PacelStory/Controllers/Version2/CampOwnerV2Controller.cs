3dc31535e91ccfe57c6142ad08871d6d73387979
maxma <mazhiguo_1219@hotmail.com>
2014-08-26 20:46:05 +0800

    Version2 准备
    Version2 准备

---
+++ PacelStory/Controllers/Version2/CampOwnerV2Controller.cs
@@ -0,0 +1,178 @@
+﻿using PacelStory.Models;
+using PacelStory.Repositories;
+using PacelStory.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PacelStory.Controllers
+{
+    [RoutePrefix("api/v2/CampOwner")]
+    public class CampOwnerV2Controller : ApiController
+    {
+        CampOwnerResponseString co_responseString = new CampOwnerResponseString();
+        CommunityResponseString communityResponseString = new CommunityResponseString();
+
+        CampOwnerRepository campOwnerRepository = new CampOwnerRepository();
+        CommunityRepository communityRepositoy = new CommunityRepository();
+        CustomerRepository customerRepository = new CustomerRepository();
+
+        // POST api/values     H5 验证用户名密码 调用此接口
+
+        [Route("CheckCampOwnerCode/{username}/{pwd}")]
+        [HttpGet]
+        //[ActionName("CheckCampOwnerCode")]
+        
+        public HttpResponseMessage CheckCampOwnerCode(string username, string pwd)
+        {
+            if (username.Trim() == "zhijian" && pwd.Trim() == "zhijian")
+            {
+                CampOwnerAndCommunity coac = new CampOwnerAndCommunity();
+                co_responseString = CommonUtility.FormatCampOwnerResponseString(0, 0, "Succeed,validate user password success");
+                return Request.CreateResponse(HttpStatusCode.OK, co_responseString);
+            }
+            else
+            {
+                co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, 0, "Failed,user password not match");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+            }
+        }
+
+        // POST api/values   H5 关联 物业 小区 时调用此接口
+        [Route("CreateCustomerIntoCommunity")]
+        [HttpPost]
+        //[ActionName("CreateCustomerIntoCommunity")]
+        public HttpResponseMessage CreateCustomerIntoCommunity([FromBody]CampOwnerAndCommunity item)
+        {
+            try
+            {
+                if (item == null || item.campOwner == null || item.community == null || item.campStaffMobileList == null || item.campStaffMobileList.Count == 0)
+                {                    
+                    co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, 0, "Failed,can not read object from body or no mobile input");
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+                }
+                else
+                {
+                    // 将社区信息 赋给 物业人员，并且创建该物业人员
+                    foreach (var mobile in item.campStaffMobileList)
+                    {
+                        Customer customer = new Customer();
+                        customer.mobile = mobile;
+                        customer.province = item.community.province;
+                        customer.city = item.community.city;
+                        customer.district = item.community.district;
+                        customer.campname = item.community.campname;
+                        customer.bldNumber = item.community.bldNumber;
+                        customer.unitNumber = item.community.unitNumber;
+                        customer.roomNumber = item.community.roomNumber;
+                        customer.type = "1";
+                        customer.communityId = item.community.communityId;
+                        customer.campCode = item.community.campCode;
+
+                        long effectedCustomerId = 0;
+                        Customer customerTemp = customerRepository.GetSpecifiedCustomerType2ByMoble(mobile);
+                        if (customerTemp == null)  // 不存在该物业， 则创建一个 customer type 2
+                        {
+                            effectedCustomerId = customerRepository.CreateCustomer(customer);
+                            if (effectedCustomerId == 0)
+                            {
+                                co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, effectedCustomerId, "Failed,can not save to the db");
+                                return Request.CreateResponse(HttpStatusCode.OK, co_responseString);
+                            }
+                        }
+                        else
+                        {
+                            // 该物业人员已经存在
+                        }
+                        
+                    }
+
+                    co_responseString = CommonUtility.FormatCampOwnerResponseString(0, 0, "Succeed,match wuye and community success!");
+                    return Request.CreateResponse(HttpStatusCode.OK, co_responseString);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, 0, "Failed," + ex.Message);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+            }
+        }
+
+
+        // POST api/values H5 创建社区 调用 此接口
+        [Route("CreateCampOwner")]
+        [HttpPost]
+        //[ActionName("CreateCampOwner")]
+        public HttpResponseMessage CreateCampOwner([FromBody]CampOwnerAndCommunity item)
+        {                                              
+            try
+            {
+                if (item == null || item.campOwner == null || item.community == null)
+                {                    
+                    co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, 0, "Failed,can not read object from body");
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+                }
+                else
+                {
+                    long effectedCampOwnerId = 0;
+
+                    // 小区owner 是否已经存在
+                    CampOwner co = campOwnerRepository.GetSpecifiedCampOwnerByMobile(item.campOwner.mobile);
+                    if (co == null)  // 不存在 去创建
+                    {
+                        item.campOwner.campCodePrefix = item.campOwner.mobile;
+                        item.campOwner.username = "zhijian";
+                        item.campOwner.password = "zhijian";
+                        effectedCampOwnerId = campOwnerRepository.CreateCampOwner(item.campOwner);
+                        if (effectedCampOwnerId == 0)
+                        {
+                            co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, effectedCampOwnerId, "Failed,can not save to the db");
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+                        }
+                        else
+                        {
+                            // 进入下面创建社区的逻辑
+                        }
+
+                    }
+                    else  // 此小区owner存在， 直接返回
+                    {
+                        // try to create community with campOwner prefix code
+
+                        // 进入下面创建社区的逻辑
+                    }
+
+                    // 创建社区
+
+                    long effectedCommunityId = 0;
+                    item.community.campCode = item.campOwner.campCodePrefix + CommonUtility.GenerateValidationCode();
+                    item.community.campOwnerMobile = item.campOwner.mobile;
+                    effectedCommunityId = communityRepositoy.CreateCommunity(item.community);
+                    if (effectedCommunityId == 0)
+                    {
+                        communityResponseString = CommonUtility.FormatCommunityResponseString(-1, effectedCommunityId, "Failed,can not save to the db");
+                        return Request.CreateResponse(HttpStatusCode.BadRequest, communityResponseString);
+                    }
+                    else
+                    {
+                        communityResponseString = CommonUtility.FormatCommunityResponseString(0, effectedCommunityId, item.community.campCode);
+                        return Request.CreateResponse(HttpStatusCode.Created, communityResponseString);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                co_responseString = CommonUtility.FormatCampOwnerResponseString(-1, 0, "Failed," + ex.Message);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, co_responseString);
+            }
+
+        }
+
+
+    }
+}
---
+++ PacelStory/Controllers/Version2/UpgradeV2Controller.cs
@@ -0,0 +1,161 @@
+﻿using PacelStory.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PacelStory.Controllers
+{
+    [RoutePrefix("api/v2/Upgrade")]
+    public class UpgradeV2Controller : ApiController
+    {
+        UpgradeResponseString urs = new UpgradeResponseString();
+
+        [Route("IsUpgradeIOS/{clientCurrentVersionNumber}")]
+        [HttpGet]
+        [Authorize]
+        public HttpResponseMessage IsUpgradeIOS(string clientCurrentVersionNumber)
+        {
+            try
+            {     
+                int currentVersionNumber = 100;
+                bool needUpgrade = false;
+
+                if (currentVersionNumber > Int16.Parse(clientCurrentVersionNumber))
+                {
+                    needUpgrade = true;
+                }
+
+                if (needUpgrade == true)
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(0, -1, "upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+                else
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "not upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+            }
+            catch
+            {
+                urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "exception occurs");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, urs);
+            }
+            
+        }
+
+        [Route("IsUpgradeAndroid/{clientCurrentVersionNumber}")]
+        [HttpGet]
+        [Authorize]
+        public HttpResponseMessage IsUpgradeAndroid(string clientCurrentVersionNumber)
+        {
+            try
+            {
+                int currentVersionNumber = 100;
+                bool needUpgrade = false;
+
+                if (currentVersionNumber > Int16.Parse(clientCurrentVersionNumber))
+                {
+                    needUpgrade = true;
+                }
+
+                if (needUpgrade == true)
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(0, -1, "upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+                else
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "not upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+            }
+            catch
+            {
+                urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "exception occurs");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, urs);
+            }
+
+        }
+
+        [Route("IsUpgradeIOSForWuye/{clientCurrentVersionNumber}")]
+        [HttpGet]
+        [Authorize]
+        public HttpResponseMessage IsUpgradeIOSForWuye(string clientCurrentVersionNumber)
+        {
+            try
+            {
+                int currentVersionNumber = 100;
+                bool needUpgrade = false;
+
+                if (currentVersionNumber > Int16.Parse(clientCurrentVersionNumber))
+                {
+                    needUpgrade = true;
+                }
+
+                if (needUpgrade == true)
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(0, -1, "upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+                else
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "not upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+            }
+            catch
+            {
+                urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "exception occurs");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, urs);
+            }
+
+        }
+
+        [Route("IsUpgradeAndroidForWuye/{clientCurrentVersionNumber}")]
+        [HttpGet]
+        [Authorize]
+        public HttpResponseMessage IsUpgradeAndroidForWuye(string clientCurrentVersionNumber)
+        {
+            try
+            {
+                int currentVersionNumber = 100;
+                bool needUpgrade = false;
+
+                if (currentVersionNumber > Int16.Parse(clientCurrentVersionNumber))
+                {
+                    needUpgrade = true;
+                }
+
+                if (needUpgrade == true)
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(0, -1, "upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+                else
+                {
+                    urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "not upgraded");
+                    return Request.CreateResponse(HttpStatusCode.OK, urs);
+                }
+            }
+            catch
+            {
+                urs = CommonUtility.FormatUpgradeResponseString(-1, -1, "exception occurs");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, urs);
+            }
+
+        }
+
+        // not use
+        private short[] String2IntArray(string versionNumber)
+        {            
+            string[] stringArray = versionNumber.Split('.');
+            short[] intArray = Array.ConvertAll(stringArray, id => Convert.ToInt16(id));
+
+            return intArray;
+        }
+    }
+}
